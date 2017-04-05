d8fed670dccc852b7c81e64f4317b87bdd15bf95
Andreas Unterberger <andreas.unterberger@student.tugraz.at>
2017-04-05 16:40:27 +0200

    [AB, AU] added join group entry at the sidebar (navigationView) added custom ExpandableListAdapter class to add test data

--- HobbIT/app/src/main/java/at/sw2017/xp4/hobit/ListHobbyGroups.java
+++ HobbIT/app/src/main/java/at/sw2017/xp4/hobit/ListHobbyGroups.java
@@ -2,6 +2,14 @@ package at.sw2017.xp4.hobit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ListHobbyGroups extends AppCompatActivity {
 
@@ -11,7 +19,21 @@ public class ListHobbyGroups extends AppCompatActivity {
         setContentView(R.layout.activity_list_hobby_groups);
         //setTitle("Individual");
 
+        ExpandableListView lv = (ExpandableListView) findViewById(R.id.expHobbyList);
 
+        //ExpandableListAdapter data = lv.getExpandableListAdapter();
+
+        ArrayList<String>listDataHeader = new ArrayList<String>();
+        HashMap<String, List<String>> listDataChild = new HashMap<String, List<String>>();
+
+        listDataHeader.add("testGroup");
+        List<String> child = new ArrayList<String>();
+        child.add("testChild");
+        listDataChild.put(listDataHeader.get(0),child);
+
+        ExpandableListAdapter listAdapter =
+                new HobbyGroupsExpListAdapter(
+                        this, listDataHeader, listDataChild);
 
     }
 }
