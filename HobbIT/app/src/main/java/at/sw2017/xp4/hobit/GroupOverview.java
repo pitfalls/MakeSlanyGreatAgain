1c6e79856c712c65835f3108bcd63bd8685f3772
Bernhard Feldhofer <f@student.tugraz.at>
2017-05-03 11:44:30 +0200

    [BD,AB] Update

--- HobbIT/app/src/main/java/at/sw2017/xp4/hobit/GroupOverview.java
+++ HobbIT/app/src/main/java/at/sw2017/xp4/hobit/GroupOverview.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class GroupOverview extends AppCompatActivity {
+
+    private String GroupHobby = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,53 @@ public class GroupOverview extends AppCompatActivity {
         //name_of_group.getStr
         setTitle("<GroupName>");
         setOnClickListeners();
+        CreateHobbySpinner();
+        CreateHobbyLocation();
+    }
+
+    public void CreateHobbySpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerHobbies);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hobbies_array, android.R.layout.simple_spinner_item);
+
+        //with Database we have to use a CoursorAdapter
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                GroupHobby = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                GroupHobby = "";
+            }
+        });
+    }
+
+    public void CreateHobbyLocation()
+    {
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerLocation);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.locations_array, android.R.layout.simple_spinner_item);
+
+        //with Database we have to use a CoursorAdapter
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                GroupHobby = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                GroupHobby = "";
+            }
+        });
 
     }
 
