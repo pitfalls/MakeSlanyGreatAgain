dbcb0938d97192d1199ef78bd16b91e3b2e9fcef
Christof Jahn <christof.jahn@student.tugraz.at>
2017-05-03 10:00:38 +0200

    Group_list_view_ test File

---
+++ HobbIt/app/src/androidTest/java/at/sw2017/xp4/hobit/GroupListViewInternalTests.java
@@ -0,0 +1,16 @@
+package at.sw2017.xp4.hobit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.Swipe;
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.Gravity;
+
+
+public class GroupListViewInternalTests {
+}
