94717d210e99bbf4d60c8fa5194a1e2d5f211cc0
GerdBerger <gerd.berger@student.tugraz.at>
2017-05-03 16:47:54 +0200

    [MP, GB] worked on edit profile page

--- HobbIT/app/src/main/java/at/sw2017/xp4/hobit/EditProfileActivity.java
+++ HobbIT/app/src/main/java/at/sw2017/xp4/hobit/EditProfileActivity.java
@@ -4,15 +4,31 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import at.sw2017.xp4.hobit.requests.GetUserRequest;
+
 public class EditProfileActivity extends AppCompatActivity {
 
     static int colorGray = 0xff888888;
+
+    private EditText editTextNickname;
+    private EditText editTextForename;
+    private EditText editTextSurename;
+    private EditText editTextLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +36,12 @@ public class EditProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profile);
 
         setOnClickListeners();
+
+        fillData();
     }
 
     private void setOnClickListeners() {
-        final EditText editTextSurename = (EditText) findViewById(R.id.editTextProfileSurename);
+       editTextSurename = (EditText) findViewById(R.id.editTextProfileSurename);
 
         //editTextSurename
         editTextSurename.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -43,7 +61,7 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
-        final EditText editTextForename = (EditText) findViewById(R.id.editTextProfileForename);
+       editTextForename = (EditText) findViewById(R.id.editTextProfileForename);
 
         editTextForename.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -61,7 +79,7 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
-        final EditText editTextNickname = (EditText) findViewById(R.id.editTextProfileNickname);
+        editTextNickname = (EditText) findViewById(R.id.editTextProfileNickname);
 
         editTextNickname.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -79,7 +97,7 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
-        final EditText editTextLocation = (EditText) findViewById(R.id.editTextProfileLocation);
+        editTextLocation = (EditText) findViewById(R.id.editTextProfileLocation);
         editTextLocation.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean bl) {
@@ -91,23 +109,6 @@ public class EditProfileActivity extends AppCompatActivity {
                     String str = editTextLocation.getText().toString();
                     if ( str.equals("") ) {
                         editTextLocation.setText("Location");
-                    }
-                }
-            }
-        });
-
-        final EditText editTextDescription = (EditText) findViewById(R.id.editTextProfileDescription);
-        editTextDescription.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean bl) {
-                if ( bl ) {
-                    editTextDescription.setText("");
-                }
-                else
-                {
-                    String str = editTextDescription.getText().toString();
-                    if ( str.equals("") ) {
-                        editTextDescription.setText("Short description of person");
                     }
                 }
             }
@@ -132,8 +133,45 @@ public class EditProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
 
+    private void fillData() {
+
+        Response.Listener<String> getUserResponseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+
+                    if (success) {
+                        editTextNickname.setText(jsonResponse.getString("NickName"));
+                        editTextForename.setText(jsonResponse.getString("FirstName"));
+                        editTextSurename.setText(jsonResponse.getString("SurName"));
+                        editTextLocation.setText(jsonResponse.getString("Location"));
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Response.ErrorListener getUserErrorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(EditProfileActivity.this);
+                builder.setMessage("Connection failed")
+                        .setNegativeButton("Retry", null)
+                        .create()
+                        .show();
+            }
+        };
+
+        GetUserRequest getUserRequest = new GetUserRequest(Globals.getInstance().getUserID(),
+                getUserResponseListener, getUserErrorListener);
+
+        RequestQueue queue = Volley.newRequestQueue(EditProfileActivity.this);
+        queue.add(getUserRequest);
+    }
 }
