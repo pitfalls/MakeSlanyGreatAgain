85f5155d73d049593b8eeba9ca22b0958e8a5bea
Bernhard Feldhofer <f@student.tugraz.at>
2017-05-10 16:10:24 +0200

    [MP,BD] Createt new Request, update Joining, soon finish.

--- HobbIT/app/src/main/java/at/sw2017/xp4/hobit/GroupOverview.java
+++ HobbIT/app/src/main/java/at/sw2017/xp4/hobit/GroupOverview.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,20 +26,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 import at.sw2017.xp4.hobit.requests.GetHobbiesDestinationRequest;
-import at.sw2017.xp4.hobit.requests.GroupCreationRequest;
 
 public class GroupOverview extends AppCompatActivity {
 
-    private void fillSpinnerArray()
+
+    private void setAdapterContent(List<String> content)
     {
-        for (int i_ = 0; i_ < iter; i_++)
-        {
-            spinnerArray[i_] = (filteredHobbies[i_]);
-        }
-      adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
+      adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, content);
     }
 
     public void printDebugToast (CharSequence text )
@@ -48,10 +46,20 @@ public class GroupOverview extends AppCompatActivity {
         save_toast.show();
     }
 
-    String[] arr, filteredHobbies;
-    int iter = 0;
-    String[] spinnerArray;
+    List<String> fullHobbyList = new ArrayList<String>();
+    List<String> hobbyList = new ArrayList<String>();
+    List<String> fullLocationList = new ArrayList<String>();
+    List<String> locationList = new ArrayList<String>();
+    List<String> fullGroupList = new ArrayList<String>();
+    List<String> groupList = new ArrayList<String>();
+
+    String currentId = "";
+    TextView descriptionView ;
+    String[][] spinnerArray;
     ArrayAdapter<String> adapter;
+    String GroupHobby;
+    Spinner spinnerHobby, spinnerLocation, spinnerGroup;
+    EditText groupInput,locationInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +70,7 @@ public class GroupOverview extends AppCompatActivity {
         setOnClickListeners();
         CreateHobbySpinner();
         CreateHobbyLocation();
+        setOnClickListeners();
 
     }
 
@@ -77,15 +86,15 @@ public class GroupOverview extends AppCompatActivity {
         return arr;
     }
 
-    public void CreateHobbySpinner() {
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerHobbies);
+    public void CreateHobbySpinner() {/*
+        spinnerHobby = (Spinner) findViewById(R.id.spinnerHobbies);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hobbies_array, android.R.layout.simple_spinner_item);
 
         //with Database we have to use a CoursorAdapter
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        spinnerHobby.setAdapter(adapter);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinnerHobby.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -96,11 +105,12 @@ public class GroupOverview extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
                 GroupHobby = "";
             }
-        });
+        });*/
+
     }
 
     public void CreateHobbyLocation()
-    {
+    {/*
         Spinner spinner = (Spinner) findViewById(R.id.spinnerLocation);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.locations_array, android.R.layout.simple_spinner_item);
 
@@ -119,69 +129,289 @@ public class GroupOverview extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
                 GroupHobby = "";
             }
-        });
+        });*/
 
     }
 
-    private void setOnClickListeners() {
-/*
-        final EditText hobby_input = (EditText) findViewById(R.id.txtview_hobby_input);
+    //----------------------------------------------------------------------------------------------
+    void HandleSpinnercontent(String groupFilter, String locationFilter)
+    {
+        hobbyList.clear();
+        locationList.clear();
+        groupList.clear();
 
-        //editTextSurename
-        hobby_input.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        if (groupFilter.equals("") && locationFilter.equals(""))
+        {
+            for (String currentHobby : fullHobbyList)
+            {
+                hobbyList.add(currentHobby);
+            }
+
+            for (String currentLocation : fullLocationList)
+            {
+                locationList.add(currentLocation);
+            }
+
+            for (String currentGroup : fullGroupList)
+            {
+                groupList.add(currentGroup);
+            }
+
+            UpdateSpinner();
+
+            currentId = spinnerArray[4][0];
+            descriptionView.setText(spinnerArray[3][0]);
+
+            return;
+        }
+
+        boolean idSet = false;
+        int idPosition = 0;
+
+        if (groupFilter.equals(""))
+        {
+            for(int iter = 0; iter < spinnerArray[0].length; iter++)
+            {
+                // [1] location
+                if (spinnerArray[1][iter].contains(locationFilter))
+                {
+                    AddToSpinner(iter);
+                    if (!idSet)
+                    {
+                        idSet = true;
+                        idPosition = iter;
+                    }
+                }
+            }
+
+        }
+        else if(locationFilter.equals(""))
+        {
+            for(int iter = 0; iter < spinnerArray[0].length; iter++)
+            {
+                // [2] group
+                if (spinnerArray[2][iter].contains(groupFilter))
+                {
+                    AddToSpinner(iter);
+                    if (!idSet)
+                    {
+                        idSet = true;
+                        idPosition = iter;
+                    }
+                }
+            }
+        }
+            else
+        {
+            for(int iter = 0; iter < spinnerArray[0].length; iter++) {
+                if (spinnerArray[1][iter].contains(locationFilter) && spinnerArray[2][iter].contains(groupFilter)) {
+                    AddToSpinner(iter);
+                    if (!idSet)
+                    {
+                        idSet = true;
+                        idPosition = iter;
+                    }
+                }
+            }
+        }
+
+        UpdateSpinner();
+
+        currentId = spinnerArray[4][idPosition];
+        descriptionView.setText(spinnerArray[3][idPosition]);
+
+    }
+
+    //----------------------------------------------------------------------------------------------
+    void UpdateSpinner()
+    {
+        setAdapterContent(hobbyList);
+        spinnerHobby.setAdapter(adapter);
+
+        setAdapterContent(locationList);
+        spinnerLocation.setAdapter(adapter);
+
+        setAdapterContent(groupList);
+        spinnerGroup.setAdapter(adapter);
+    }
+
+    //----------------------------------------------------------------------------------------------
+    void AddToSpinner(int iter)
+    {
+        if (!hobbyList.contains(spinnerArray[0][iter]))
+        {
+            hobbyList.add(spinnerArray[0][iter]);
+        }
+        if (!locationList.contains(spinnerArray[1][iter]))
+        {
+            locationList.add(spinnerArray[1][iter]);
+        }
+        if (!groupList.contains(spinnerArray[2][iter]))
+        {
+            groupList.add(spinnerArray[2][iter]);
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    private void setOnClickListeners() {
+
+        groupInput    = (EditText) findViewById(R.id.txtGroupText);
+        locationInput = (EditText) findViewById(R.id.txtview_location_input);
+
+        groupInput.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onFocusChange(View v, boolean bl) {
-                hobby_input.setText("--------");
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                HandleSpinnercontent(groupInput.getText().toString(), locationInput.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
-    //    printDebugToast("hallosssAS");
-*/
-        //##########################################################################################
 
+        //---------------------------------------------------------
+        locationInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                HandleSpinnercontent(groupInput.getText().toString(), locationInput.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        //------------------------------------------------------------------------------------------
+
+        Response.Listener<String> joinResponseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        };
+
+        Response.ErrorListener joinErrorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(GroupOverview.this);
+                builder.setMessage("you are not good enough to join!! ;-)")
+                        .setNegativeButton("Retry", null)
+                        .create()
+                        .show();
+            }
+        };
+
+        //------------------------------------------------------------------------------------------ responseListener
         Response.Listener<String> responseListener = new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
-                    printDebugToast("hallo");
+                   // printDebugToast("hallo");
 
                     JSONObject jsonResponse = new JSONObject(response);
-                    boolean success = jsonResponse.getBoolean("success");
-                    final JSONArray allHobbies = jsonResponse.getJSONArray("hobby");
+                    final JSONArray allHobbies         = jsonResponse.getJSONArray("hobby");
+                    final JSONArray allLocations       = jsonResponse.getJSONArray("location");
+                    final JSONArray allGroups          = jsonResponse.getJSONArray("group");
+                    final JSONArray allDescritpions    = jsonResponse.getJSONArray("description");
+                    final JSONArray allIds             = jsonResponse.getJSONArray("id");
 
                     //---------------------------------------------------------------------------------- CODE
                     //----------------------------------------------------------------------------------
 
-                    arr = toStringArray(allHobbies);
-                    filteredHobbies = new String[arr.length];
-                    filteredHobbies[0] = arr[0];
-
-                    for (int i = 0; i < allHobbies.length(); i++) {
-
-                        //printDebugToast(arr[i]);
-                     if(!filteredHobbies[iter].contains(arr[i]))
-                     {
-                         iter++;
-                         filteredHobbies[iter] = arr[i];
-                         printDebugToast(arr[i]);
-                     }
+                    for (int i = 0; i < allHobbies.length(); i++)
+                    {
+                        if(!fullHobbyList.contains(allHobbies.get(i)))
+                        {
+                            fullHobbyList.add( allHobbies.getString(i) );
+                        }
                     }
 
-                    printDebugToast("Hier seien nun ich!!");
+                    for (int i = 0; i < allLocations.length(); i++)
+                    {
+                        if(!fullLocationList.contains(allLocations.get(i)))
+                        {
+                            fullLocationList.add( allLocations.getString(i) );
+                        }
+                    }
 
-                    ////////////////////////////////////////////////////////////////////////////////////////////
-                    // Spinner füllen
+                    for (int i = 0; i < allGroups.length(); i++)
+                    {
+                        if(!fullGroupList.contains(allGroups.get(i)))
+                        {
+                            fullGroupList.add( allGroups.getString(i) );
+                        }
+                    }
 
-                    spinnerArray = new String[arr.length];
-                    fillSpinnerArray();
-                    Spinner s = (Spinner) findViewById(R.id.spinnerHobbies);
+                    spinnerArray = new String[5][allHobbies.length()];
 
-                    TextView textView = (TextView) findViewById(R.id.txtview_description_input);
-                    textView.setText(Arrays.toString(spinnerArray));
+                    for (int i = 0; i < allLocations.length(); i++)
+                    {
+                        spinnerArray[0][i] = allHobbies.getString(i);
+                        spinnerArray[1][i] = allLocations.getString(i);
+                        spinnerArray[2][i] = allGroups.getString(i);
+                        spinnerArray[3][i] = allDescritpions.getString(i);
+                        spinnerArray[4][i] = String.valueOf(allIds.getInt(i));
+                    }
 
+
+                    descriptionView = (TextView) findViewById(R.id.txtview_description_input);
+
+                    spinnerHobby = (Spinner) findViewById(R.id.spinnerHobbies);
+                    setAdapterContent(fullHobbyList);
                     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    s.setAdapter(adapter);
+                    spinnerHobby.setAdapter(adapter);
 
-                    printDebugToast("Juuunge fertisch!");
+                    spinnerLocation = (Spinner) findViewById(R.id.spinnerLocation);
+                    setAdapterContent(fullLocationList);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinnerLocation.setAdapter(adapter);
+
+                    spinnerGroup = (Spinner) findViewById(R.id.spinnerGroup);
+                    setAdapterContent(fullGroupList);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinnerGroup.setAdapter(adapter);
+                    spinnerGroup.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            String item = spinnerGroup.getSelectedItem().toString();
+
+                            for (int iter = 0; iter < spinnerArray[0].length; iter++)
+                            {
+                                if (item.equals(spinnerArray[2][iter]))
+                                {
+                                    currentId = spinnerArray[4][iter];
+                                    descriptionView.setText(spinnerArray[3][iter]);
+                                    return;
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+
+                    currentId = spinnerArray[4][0];
+
+                    descriptionView.setText(spinnerArray[3][0]);
+
+                  //  printDebugToast("Juuunge fetish!");
 
                     //----------------------------------------------------------------------------------
                     //----------------------------------------------------------------------------------
@@ -196,6 +426,10 @@ public class GroupOverview extends AppCompatActivity {
 
             }
         };
+
+
+
+
 
         Response.ErrorListener errorListener = new Response.ErrorListener() {
             @Override
--- HobbIT/app/src/main/java/at/sw2017/xp4/hobit/requests/joinGroupRequest.java
+++ HobbIT/app/src/main/java/at/sw2017/xp4/hobit/requests/joinGroupRequest.java
@@ -1,8 +1,31 @@
 package at.sw2017.xp4.hobit.requests;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by anon on 10.05.17.
  */
 
-public class joinGroupRequest {
+public class joinGroupRequest extends StringRequest{
+
+    private static final String JOIN_REQUEST_URL = "https://waterproofed-school.000webhostapp.com/JoinGroup.php";
+    private Map<String, String> params;
+
+    public  JoinGroupRequest(String userId, String groupId,Response.Listener<String> listener, Response.ErrorListener errorListener){
+        super(Request.Method.POST, JOIN_REQUEST_URL, listener, errorListener);
+        params = new HashMap<>();
+        params.put("userID", userId);
+        params.put("groupID", groupId);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+
 }
