9288c4c30b2331b1e57b9f5aae6d2c6ba78f6b1c
Christof Jahn <christof.jahn@student.tugraz.at>
2017-05-10 14:16:25 +0200

    [CJ, AB] new startscreen

---
+++ HobbIT/app/src/main/java/at/sw2017/xp4/hobit/HobbiT_Main_Startscreen.java
@@ -0,0 +1,44 @@
+package at.sw2017.xp4.hobit;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Christof on 10.05.2017.
+ */
+
+public class HobbiT_Main_Startscreen extends AppCompatActivity {
+
+    private static final HobbiT_Main_Startscreen ourInstance = new HobbiT_Main_Startscreen();
+
+    public static HobbiT_Main_Startscreen getInstance() {
+        return ourInstance;
+    }
+
+    public HobbiT_Main_Startscreen()
+    {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hobbit_startscreen);
+
+        /*final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+            }
+        }, 5000000);
+           */
+        Intent login = new Intent(this, FacebookLogin.class);
+        startActivityForResult(login, 1);
+
+        finish();
+    }
+}
