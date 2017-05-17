4dff8b0cbe65da738eba0882bf6a6be0e2371938
Andreas Unterberger <andreas.unterberger@student.tugraz.at>
2017-05-17 15:29:23 +0200

    [AU,MW] added database connection for the HobbyGroupListActivity

--- HobbIT/app/src/main/java/at/sw2017/xp4/hobit/HobbyGroupListActivity.java
+++ HobbIT/app/src/main/java/at/sw2017/xp4/hobit/HobbyGroupListActivity.java
@@ -5,13 +5,26 @@ package at.sw2017.xp4.hobit;
  */
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.widget.ExpandableListView;
 import android.widget.SearchView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import at.sw2017.xp4.hobit.requests.GetUserGroupsRequest;
 
 public class HobbyGroupListActivity extends Activity implements SearchView.OnQueryTextListener, SearchView.OnCloseListener {
 
@@ -19,6 +32,9 @@ public class HobbyGroupListActivity extends Activity implements SearchView.OnQue
     private HobbyGroupListAdapter listAdapter;
     private ExpandableListView myList;
     private ArrayList<HobbyData> hobbyList = new ArrayList<HobbyData>();
+
+    List<String> allUserHobbies = new ArrayList<String>();
+    String[][] groupDataList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,13 +49,84 @@ public class HobbyGroupListActivity extends Activity implements SearchView.OnQue
         search.setOnCloseListener(this);
 
         //display the list
-        displayList();
+        //displayList();
         //expand all Groups
-        expandAll();
-
+        //expandAll();
+        setupDatabaseConnection();
     }
 
 
+    public void setupDatabaseConnection()
+    {
+        Response.Listener<String> listener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+
+                    if (success) {
+
+                        final JSONArray allHobbies = jsonResponse.getJSONArray("Hobbies");
+                        final JSONArray allNames = jsonResponse.getJSONArray("Names");
+                        final JSONArray allIds = jsonResponse.getJSONArray("GroupIDs");
+
+
+
+                        groupDataList = new String[3][allIds.length()];
+
+                        for (int i = 0; i < groupDataList[0].length; i++)
+                        {
+
+                            String currentHobby = allHobbies.getString(i);
+                            if(!allUserHobbies.contains(currentHobby))
+                            {
+                                allUserHobbies.add(currentHobby);
+                            }
+
+                            groupDataList[0][i] = allIds.getString(i);
+                            groupDataList[1][i] = allNames.getString(i);
+                            groupDataList[2][i] = currentHobby;
+
+
+                        }
+
+                        //display the list
+                        displayList();
+                        //expand all Groups
+                        expandAll();
+
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(HobbyGroupListActivity.this);
+                        builder.setMessage("Something went wrong!")
+                                .setNegativeButton("OK", null)
+                                .create()
+                                .show();
+                    }
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(HobbyGroupListActivity.this);
+                builder.setMessage("Connection failed")
+                        .setNegativeButton("Retry", null)
+                        .create()
+                        .show();
+            }
+        };
+        GetUserGroupsRequest getUserGroupsRequest = new GetUserGroupsRequest(listener, errorListener);
+        final RequestQueue queue = Volley.newRequestQueue(HobbyGroupListActivity.this);
+        queue.add(getUserGroupsRequest);
+
+    }
 
     //@Override
    /*public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,7 +146,7 @@ public class HobbyGroupListActivity extends Activity implements SearchView.OnQue
     private void displayList() {
 
         //display the list
-        loadSomeData();
+        loadDataFromDatabase();
 
         //get reference to the ExpandableListView
         myList = (ExpandableListView) findViewById(R.id.expandableList);
@@ -70,7 +157,7 @@ public class HobbyGroupListActivity extends Activity implements SearchView.OnQue
 
     }
 
-    private void loadSomeData() {
+    private void loadSomeTestData() {
 
         ArrayList<GroupData> groupList = new ArrayList<GroupData>();
         GroupData group = new GroupData("TEST",10000000);
@@ -94,6 +181,29 @@ public class HobbyGroupListActivity extends Activity implements SearchView.OnQue
         hobby = new HobbyData("Asia",groupList);
         hobbyList.add(hobby);
 
+    }
+
+    private void loadDataFromDatabase()
+    {
+        ArrayList<GroupData> groupList;
+        GroupData group;
+        HobbyData hobby;
+
+        for(String currentHobby : allUserHobbies){
+            groupList = new ArrayList<>();
+
+            for(int iter = 0; iter < groupDataList[0].length; iter++){
+
+                if(groupDataList[2][iter].equals(currentHobby)){
+
+                    group = new GroupData(groupDataList[1][iter],Integer.parseInt( groupDataList[0][iter]));
+                    groupList.add(group);
+                }
+            }
+            hobby = new HobbyData(currentHobby, groupList);
+            hobbyList.add(hobby);
+
+        }
     }
 
     @Override
