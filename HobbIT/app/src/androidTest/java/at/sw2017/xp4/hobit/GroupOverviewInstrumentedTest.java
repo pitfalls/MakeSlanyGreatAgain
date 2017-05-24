57b4ec8b7929ff7099a96833620d55e7d49ec39a
Bernhard Feldhofer <f@student.tugraz.at>
2017-05-24 14:41:03 +0200

    [CJ,BD] update of testcase, error was in, testcoverage higher

--- HobbIT/app/src/androidTest/java/at/sw2017/xp4/hobit/GroupOverviewInstrumentedTest.java
+++ HobbIT/app/src/androidTest/java/at/sw2017/xp4/hobit/GroupOverviewInstrumentedTest.java
@@ -1,6 +1,7 @@
 
 package at.sw2017.xp4.hobit;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -14,10 +15,15 @@ import java.security.acl.Group;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.isDialog;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
@@ -37,26 +43,100 @@ public class GroupOverviewInstrumentedTest {
 
 
     @Test
-    public void testContentExistence() throws Exception {
+    public void testSpinners() throws Exception {
 
-        //onView(withText("JOIN")).perform(click());
-
-
-       /* ViewInteraction customTextView = onView(allOf (withId(R.id.tv_spinner_desc), withText("hello"), isDisplayed()));
-        customTextView.perform(click());
-*/
-       // Spinner mySpinner = (Spinner) findViewById(R.id.my_spinner);
-      //  List<MyValue> list = Arrays.asList(
+        //get Data from DB
+        Thread.sleep(3000);
 
         onView(withId(R.id.spinnerHobbies)).perform(click());
-        onData(allOf(is(instanceOf(String.class)), is(selectionText))).perform(click());
-        onView(withId(spinnerId)).check(matches(withSpinnerText(containsString(selectionText))));
+        onData(allOf(is(instanceOf(String.class)), is("Football"))).perform(click());
+        onView(withId(R.id.spinnerHobbies)).check(matches(withSpinnerText(containsString("Football"))));
 
-        onView(withId(R.id.btn_join)).perform(click());
-        onView(withId(R.id.btn_join)).perform(click());
-        onView(withId(R.id.btn_join)).perform(click());
+        onView(withId(R.id.spinnerHobbies)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Tennis"))).perform(click());
+        onView(withId(R.id.spinnerHobbies)).check(matches(withSpinnerText(containsString("Tennis"))));
+
+        onView(withId(R.id.spinnerHobbies)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Cycling"))).perform(click());
+        onView(withId(R.id.spinnerHobbies)).check(matches(withSpinnerText(containsString("Cycling"))));
+
+        Thread.sleep(1500);
+
+        onView(withId(R.id.spinnerLocation)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Graz"))).perform(click());
+        onView(withId(R.id.spinnerLocation)).check(matches(withSpinnerText(containsString("Graz"))));
+
+        onView(withId(R.id.spinnerLocation)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Wien"))).perform(click());
+        onView(withId(R.id.spinnerLocation)).check(matches(withSpinnerText(containsString("Wien"))));
+
+        onView(withId(R.id.spinnerLocation)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Salzburg"))).perform(click());
+        onView(withId(R.id.spinnerLocation)).check(matches(withSpinnerText(containsString("Salzburg"))));
 
 
+        onView(withId(R.id.spinnerLocation)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Innsbruck"))).perform(click());
+        onView(withId(R.id.spinnerLocation)).check(matches(withSpinnerText(containsString("Innsbruck"))));
+
+        Thread.sleep(1500);
+
+        onView(withId(R.id.spinnerGroup)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("TestGroup1"))).perform(click());
+        onView(withId(R.id.spinnerGroup)).check(matches(withSpinnerText(containsString("TestGroup1"))));
+        onView(withId(R.id.txtview_description_input)).check(matches(withText("Test test test test")));
+        onView(withId(R.id.btn_join)).perform(click());
+        Thread.sleep(1500);
+        onView(withText("You are already member of this group :-)")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(withText("Ok")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
+
+        onView(withId(R.id.spinnerGroup)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("TennisWien"))).perform(click());
+        onView(withId(R.id.spinnerGroup)).check(matches(withSpinnerText(containsString("TennisWien"))));
+        onView(withId(R.id.txtview_description_input)).check(matches(withText("Playing tennis in vienna")));
+        onView(withId(R.id.btn_join)).perform(click());
+        Thread.sleep(1500);
+        onView(withText("You are already member of this group :-)")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(withText("Ok")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
+
+        onView(withId(R.id.spinnerGroup)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("CyclingSalzburg"))).perform(click());
+        onView(withId(R.id.spinnerGroup)).check(matches(withSpinnerText(containsString("CyclingSalzburg"))));
+        onView(withId(R.id.txtview_description_input)).check(matches(withText("Cycling in Salzburg :)")));
+        onView(withId(R.id.btn_join)).perform(click());
+        Thread.sleep(1500);
+        onView(withText("You are already member of this group :-)")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(withText("Ok")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
+
+        onView(withId(R.id.spinnerGroup)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("FootballInnsbruck"))).perform(click());
+        onView(withId(R.id.spinnerGroup)).check(matches(withSpinnerText(containsString("FootballInnsbruck"))));
+        onView(withId(R.id.txtview_description_input)).check(matches(withText("Playing footbal in Innsbruck")));
+        onView(withId(R.id.btn_join)).perform(click());
+        Thread.sleep(1500);
+        onView(withText("You are already member of this group :-)")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(withText("Ok")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
 
     }
+      /*  @Test
+        public void testFilter() throws Exception {
+
+            Thread.sleep(7500);
+         /*
+        onView(withId(R.id.txtview_location_input)).perform(replaceText("shitshitshit"));
+        onView(withId(R.id.spinnerGroup)).check(matches(withSpinnerText("")));
+        onView(withId(R.id.spinnerLocation)).check(matches(withSpinnerText("")));
+        onView(withId(R.id.spinnerGroup)).check(matches(withSpinnerText("")));
+        onView(withId(R.id.txtview_location_input)).perform(replaceText(""));
+               Thread.sleep(2000);
+        onView(withId(R.id.txtGroupText)).perform(replaceText("shitshitshit"));
+        onView(withId(R.id.spinnerHobbies)).check(matches(withSpinnerText("")));
+        onView(withId(R.id.spinnerLocation)).check(matches(withSpinnerText("")));
+        onView(withId(R.id.spinnerGroup)).check(matches(withSpinnerText("")));
+
+        //onView(withId(R.id.)).perform(replaceText("shitshitshit"));
+
+
+
+    }   */
 }
