034a2771fb33be52c60cde698c2757dc5296f9d8
brkic <brkic@users.noreply.github.com>
2017-06-07 10:25:43 +0200

    MW,AB
    moved overview button

--- HobbIT/app/src/main/java/at/sw2017/xp4/hobit/HobIT_Main.java
+++ HobbIT/app/src/main/java/at/sw2017/xp4/hobit/HobIT_Main.java
@@ -241,11 +241,11 @@ public class HobIT_Main extends AppCompatActivity
             Intent intent = new Intent(HobIT_Main.this, HobbyGroupListActivity.class);
             startActivity(intent);
         }
-        else if (id == R.id.group_overview) {
+        /*else if (id == R.id.group_overview) {
             Intent intent = new Intent(HobIT_Main.this, GroupOverview.class);
             startActivity(intent);
 
-        }
+        }*/
         else if (id == R.id.group_creation) {
             Intent intent = new Intent(HobIT_Main.this, GroupCreation.class);
             startActivity(intent);
